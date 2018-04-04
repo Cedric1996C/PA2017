0e13febf2f959ccc4fcbdbbee173de9eef66640c
tracer-pa2017 <tracer@njuics.org>
2018-04-04 10:19:26 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  10:19:26 up  9:17,  1 user,  load average: 0.01, 0.03, 0.01 f38e337798a3b92f8ad53f0f6e0d0caa91f123d3

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -45,9 +45,8 @@ void set_CF_adc(uint32_t result, uint32_t src){
 }
 
 uint32_t alu_adc(uint32_t src, uint32_t dest) {
-  uint32_t result = src + dest;
+  uint32_t result = src + dest + cpu.eflags.CF;
   set_CF_adc(result, src);
-  result += cpu.eflags.CF;
   set_PF(result);
   set_ZF(result);
   set_SF(result);
