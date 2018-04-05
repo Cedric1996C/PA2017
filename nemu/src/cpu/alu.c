71cafdc9474cdf8ea66988ec6f20e73ab39cc609
tracer-pa2017 <tracer@njuics.org>
2018-04-05 19:46:09 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  19:46:09 up 1 day,  4:39,  1 user,  load average: 0.32, 0.11, 0.08 2560ef15e2288d7cdd75546f5b49618c9597b0

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -138,14 +138,15 @@ int64_t alu_imul(int32_t src, int32_t dest, size_t data_size) {
 
 uint32_t alu_div(uint64_t src, uint64_t dest, size_t data_size) {
   if(src==0) return 0;
+  uint64_t low_16_bits,low_32_bits;
   switch(data_size){
     case 8:
-      uint64_t low_16_bits = dest & 0xffff;
+      low_16_bits = dest & 0xffff;
       src = src & 0xff;
       dest = low_16_bits / src;
       break;
     case 16:
-      uint64_t low_32_bits = dest & 0xffffffff;
+      low_32_bits = dest & 0xffffffff;
       src = src & 0xffff;
       dest = low_16_bits / src;
       break;
