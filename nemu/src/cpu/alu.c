c3e891670347309669cd65d394bd92d7adf3c50b
tracer-pa2017 <tracer@njuics.org>
2018-04-05 10:52:51 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  10:52:51 up 20:08,  1 user,  load average: 0.02, 0.12, 0.08 411896baa526a3a9b644313b9f169bb27665e246

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -162,15 +162,33 @@ int32_t alu_imod(int64_t src, int64_t dest) {
 }
 
 uint32_t alu_and(uint32_t src, uint32_t dest) {
-	return src & dest;
+  uint32_t result = src & dest;
+  set_PF(result);
+  set_SF(result);
+  set_ZF(result);
+  cpu.eflags.CF = 0;
+  cpu.eflags.OF = 0;
+	return result;
 }
 
 uint32_t alu_xor(uint32_t src, uint32_t dest) {
-	return src ^ dest;
+  uint32_t result = src ^ dest;
+  set_PF(result);
+  set_SF(result);
+  set_ZF(result);
+  cpu.eflags.CF = 0;
+  cpu.eflags.OF = 0;
+	return result;
 }
 
 uint32_t alu_or(uint32_t src, uint32_t dest) {
-	return src | dest;
+  uint32_t result = src | dest;
+  set_PF(result);
+  set_SF(result);
+  set_ZF(result);
+  cpu.eflags.CF = 0;
+  cpu.eflags.OF = 0;
+	return result;
 }
 
 uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size) {
