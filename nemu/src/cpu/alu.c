ba3a9209d56bf62880d39cafd019c109240a3ba0
tracer-pa2017 <tracer@njuics.org>
2018-04-05 16:16:19 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  16:16:19 up 1 day,  1:09,  1 user,  load average: 0.49, 0.35, 0.21 cdb3dba8639ea3c2a3b143b5f62370137427c310

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -218,7 +218,7 @@ uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size) {
     if(src==1){
       cpu.eflags.OF = OF_flag == CF_flag ? 0:1;
     }
-    result << src;
+    result <<= src;
     set_PF(dest);
     set_SF(result);
     set_ZF(result);
