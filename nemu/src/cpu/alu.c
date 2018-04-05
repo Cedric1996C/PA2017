db27b879b916fac634530e041290c651ad40de3a
tracer-pa2017 <tracer@njuics.org>
2018-04-05 10:42:43 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  10:42:43 up 19:58,  1 user,  load average: 0.12, 0.11, 0.04 db388ae5d285f8993bbbe7c1757ae6d437e62fa0

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -109,15 +109,15 @@ uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size) {
   bool res;
   switch(data_size){
     case 8:
-      uint8_t high_8_bits = result & 0xff00 >> 8;
+      uint8_t high_8_bits = (result & 0xff00) >> 8;
       res = high_8_bits ? 1:0;
       break;
     case 16:
-      uint16_t high_16_bits = result & 0xffff0000 >> 16;
+      uint16_t high_16_bits = (result & 0xffff0000) >> 16;
       res = high_16_bits ? 1:0;
       break;
     case 32:
-      uint32_t high_32_bits = result & 0xffffffff00000000 >> 32;
+      uint32_t high_32_bits = (result & 0xffffffff00000000) >> 32;
       res = high_32_bits ? 1:0;
       break;
     default:
