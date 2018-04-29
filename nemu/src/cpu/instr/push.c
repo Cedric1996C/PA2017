a26cf2f01252dc13e66b73f20e46d81cb061c605
tracer-pa2017 <tracer@njuics.org>
2018-04-29 20:59:32 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  20:59:32 up 6 days, 13:06,  1 user,  load average: 0.68, 0.29, 0.09 641c12ec5146b7bcfd1ef41444de5ce5dd4f8b0b

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -6,10 +6,10 @@ make_instr_func(push_ebp_v)
   cpu.esp -= data_size / 8;
   int len = 1;
 
-  temp_ebp.type = OPR_REG;
+  temp_ebp.type = OPR_MEM;
   temp_ebp.data_size = data_size;
   temp_ebp.val = cpu.ebp;
-  // temp_ebp.sreg = SREG_SS;
+  temp_ebp.sreg = SREG_SS;
   temp_ebp.addr = cpu.esp;
 
   operand_write(&temp_ebp);
