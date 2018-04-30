13cf21a95929838223522db4b0f3077a62cb1f56
tracer-pa2017 <tracer@njuics.org>
2018-04-30 20:44:13 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  20:44:13 up 6 days, 23:41,  1 user,  load average: 0.09, 0.22, 0.14 672061efdbff32c9b893f2bcd400d497d3afee6

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -3,17 +3,18 @@
 make_instr_func(ret_v)
 {
   OPERAND rel;
-  cpu.esp += data_size / 8;
 
   rel.type = OPR_MEM;
   rel.addr = cpu.esp;
   rel.data_size = data_size;
   operand_read(&rel);
+  cpu.esp += data_size / 8;
   if(data_size == 16){
     cpu.eip = rel.val & 0x0000ffff;
   } else {
     cpu.eip = rel.val;
   }
+  
   return 0;
 }
 
