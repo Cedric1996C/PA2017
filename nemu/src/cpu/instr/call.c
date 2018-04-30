48d4e3926bfa24dcde8fab61de6b2bd526724f7e
tracer-pa2017 <tracer@njuics.org>
2018-04-30 21:04:23 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  21:04:23 up 7 days, 1 min,  1 user,  load average: 0.07, 0.02, 0.02 f973382ff7f66ca6d935386a97ede5133bd1f93c

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -1,55 +1,28 @@
 #include "cpu/instr.h"
 
-// make_instr_func(call_rel_v)
-// {
-//   OPERAND temp_eip, rel;
-//   int data_byte = data_size / 8;
-//   int len = 1 + data_byte;
-//   cpu.esp -= data_byte;
-
-//   temp_eip.type = OPR_MEM;
-//   temp_eip.data_size = data_size;
-//   temp_eip.val = cpu.eip;
-//   temp_eip.addr = cpu.esp;
-//   operand_write(&temp_eip);
-
-//   rel.type = OPR_IMM;
-//   rel.data_size = data_size;
-//   rel.addr = eip + 1;
-//   operand_read(&rel);
-
-//   if(data_size == 16){
-//     cpu.eip = (cpu.eip + rel.val)&0x0000ffff;
-//   } else {
-//     cpu.eip += rel.val;
-//   }
-//   return len;
-// }
-
 make_instr_func(call_rel_v)
 {
-  OPERAND opr_eip, imm;
-  int len = 1 + data_size / 8;
-  cpu.esp -= data_size / 8;
+  OPERAND temp_eip, rel;
+  int data_byte = data_size / 8;
+  int len = 1 + data_byte;
+  cpu.esp -= data_byte;
 
-  opr_eip.type = OPR_MEM;
-  opr_eip.data_size = data_size;
-  opr_eip.val = eip + len;
-  opr_eip.addr = cpu.esp;
-  opr_eip.sreg = SREG_SS;
-  operand_write(&opr_eip);
+  temp_eip.type = OPR_MEM;
+  temp_eip.data_size = data_size;
+  print_asm_1("call","",len,&temp_eip);
+  temp_eip.val = cpu.eip;
+  temp_eip.addr = cpu.esp;
+  operand_write(&temp_eip);
 
-  imm.data_size = data_size;
-  imm.type = OPR_IMM;
-  imm.addr = eip + 1;
-  operand_read(&imm);
-  print_asm_1("call", "", len, &imm);
+  rel.type = OPR_IMM;
+  rel.data_size = data_size;
+  rel.addr = eip + 1;
+  operand_read(&rel);
 
-  assert(data_size == 16 || data_size == 32);
-  if (data_size == 16)
-  {
-    imm.val = (int32_t)(imm.val << 16) >> 16;
+  if(data_size == 16){
+    cpu.eip = (cpu.eip + rel.val)&0x0000ffff;
+  } else {
+    cpu.eip += rel.val;
   }
-  len += (int32_t)imm.val;
   return len;
 }
