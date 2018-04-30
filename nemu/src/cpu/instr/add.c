a78680e0dc7094186d96a4734372218096d184c0
tracer-pa2017 <tracer@njuics.org>
2018-04-30 21:51:19 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  21:51:19 up 7 days, 48 min,  1 user,  load average: 0.96, 0.20, 0.06 4ddda38cec89fe893ce4a8bfcdaed1658f1b696f

---
+++ nemu/src/cpu/instr/add.c
@@ -0,0 +1,19 @@
+#include "cpu/instr.h"
+
+make_instr_func(add_i2rm_v)
+{
+  OPERAND imm, rm;
+  int len = 1;
+  int data_byte = data_size / 8;
+  len += modrm_opcode_rm(eip + 1, &opcode, &rm);
+
+  imm.type = OPR_IMM;
+  imm.data_size = data_size;
+  imm.addr = eip + len;
+  operand_read(&imm);
+  operand_read(&rm);
+  print_asm_2("add", "", len + data_byte, &imm, &rm);
+  rm.val = alu_add(imm.val, rm.val);
+
+  return len;
+}
