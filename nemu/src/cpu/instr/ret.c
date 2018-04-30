abb90d3a31b1fa0a3347943947ffd9e9d6c6051c
tracer-pa2017 <tracer@njuics.org>
2018-04-30 20:38:50 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  20:38:50 up 6 days, 23:36,  1 user,  load average: 0.18, 0.12, 0.06 d303bd3e76c6a1629a4da81ef2b6c8d1c3c3ab42

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -1,18 +1,40 @@
 #include "cpu/instr.h"
 
+// make_instr_func(ret_v)
+// {
+//   OPERAND rel;
+//   cpu.esp -= data_size / 8;
+
+//   rel.type = OPR_MEM;
+//   rel.addr = cpu.esp;
+//   rel.data_size = data_size;
+//   operand_read(&rel);
+//   if(data_size == 16){
+//     cpu.eip = rel.val & 0x0000ffff;
+//   } else {
+//     cpu.eip = rel.val;
+//   }
+//   return 0;
+// }
+
 make_instr_func(ret_v)
 {
-  OPERAND rel;
-  cpu.esp -= data_size / 8;
+  OPERAND addr;
+  int len = 1;
 
-  rel.type = OPR_MEM;
-  rel.addr = cpu.esp;
-  rel.data_size = data_size;
-  operand_read(&rel);
-  if(data_size == 16){
-    cpu.eip = rel.val & 0x0000ffff;
-  } else {
-    cpu.eip = rel.val;
-  }
+  addr.type = OPR_MEM;
+  addr.data_size = data_size;
+  addr.addr = cpu.esp;
+  addr.sreg = SREG_SS;
+  operand_read(&addr);
+  cpu.esp += data_size / 8;
+  print_asm_0("ret", "", len);
+
+  assert(data_size == 16 || data_size == 32);
+  if (data_size == 16)
+    cpu.eip = addr.val & 0xffff;
+  else
+    cpu.eip = addr.val;
+
   return 0;
 }
