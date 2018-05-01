502abf4d33460b177f94bbb840fbaf8d3ddaf05c
tracer-pa2017 <tracer@njuics.org>
2018-05-01 16:43:42 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  16:43:42 up 7 days,  7:23,  1 user,  load average: 1.12, 0.25, 0.11 c7685cbdf46f7215127d9868cc0613b5514cf31a

--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -6,7 +6,7 @@ make_instr_func(sub_i2rm_bv)
   int len = 1;
   
   rm.data_size = data_size;
-  len += modrm_opcode_rm(eip+1, opcode, &rm);
+  len += modrm_opcode_rm(eip+1, &opcode, &rm);
   operand_read(&rm);
   
   imm.type = OPR_IMM;
