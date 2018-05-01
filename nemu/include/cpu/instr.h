3fc81508edac103bb271ec7d63f1c7944113b945
tracer-pa2017 <tracer@njuics.org>
2018-05-01 14:51:37 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  14:51:37 up 7 days,  5:31,  1 user,  load average: 0.16, 0.03, 0.01 2cc47b6753fd4dce7a8ea00e7281b9a6d0c30d7

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -28,5 +28,6 @@ int push_ebp_v(uint32_t eip, uint8_t opcode);
 int call_rel_v(uint32_t eip, uint8_t opcode);
 int ret_v(uint32_t eip, uint8_t opcode);
 int add_i2rm_v(uint32_t eip, uint8_t opcode);
+int test_r2rm_v(uint32_t eip, uint8_t opcode);
 
 #endif
---
+++ nemu/src/cpu/instr/test.c
@@ -0,0 +1,17 @@
+#include "cpu/instr.h"
+
+make_instr_func(test_r2rm_v)
+{
+  OPERAND reg, rm;
+  int len = 1;
+  
+  reg.data_size = data_size;
+  rm.data_size = data_size;
+  len += modrm_r_rm(eip, &reg, &rm);
+  operand_read(&reg);
+  operand_read(&rm);
+  print_asm_2("test", "", len, &reg, &rm);
+  alu_add(reg.val, rm.val);
+  
+  return len;
+}
