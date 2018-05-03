284df356c5a990407b0ad7ec160bd5e3e16e0ef6
tracer-pa2017 <tracer@njuics.org>
2018-05-03 15:57:51 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  15:57:51 up 8 days,  5:09,  1 user,  load average: 0.83, 0.32, 0.20 f3e3de561628bd53be30be5c3a91e927f466614

--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -24,14 +24,14 @@ make_instr_func(and_i2rm_bv)
   imm.type = OPR_IMM;
   imm.addr = eip+len;
   operand_read(&imm);
-  imm.data_size = data_size;
+  // imm.data_size = data_size;
   len += 1;
 
   if(data_size == 16)
     imm.val = (uint16_t)(imm.val << 8) >> 8;
   else
     imm.val = (uint32_t)(imm.val << 24) >> 24;
-    
+
   rm.val = alu_and(imm.val, rm.val);
   print_asm_2("and", "", len, &imm, &rm);
   operand_write(&rm);
