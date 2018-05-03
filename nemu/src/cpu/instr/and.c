afe0636a25215108abe21cf92ad0fe0be41425d4
tracer-pa2017 <tracer@njuics.org>
2018-05-03 15:39:55 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  15:39:55 up 8 days,  4:51,  1 user,  load average: 1.04, 0.23, 0.07 23bcb9e9cf61b2458aa70fb4103943914a0abaa

--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -10,4 +10,30 @@ static void instr_execute_2op_and()
   cpu.eflags.CF = 0;
 }
 
-make_instr_impl_2op_and(i, rm, bv);
+// make_instr_impl_2op_and(i, rm, bv);
+
+make_instr_func(and_i2rm_bv)
+{
+  OPERAND imm, rm;
+  int len = 1;
+  imm.data_size = 8;
+  rm.data_size = data_size;
+
+  len += modrm_opcode_rm(eip+len, &opcode, &rm);
+  operand_read(&rm);
+  imm.type = OPR_IMM;
+  imm.addr = eip+len;
+  operand_read(&imm);
+  len += 1;
+  print_asm_2("add", "", len, &imm, &rm);
+
+  if(data_size == 16){
+    imm.val = (uint16_t)(imm.val << 8) >> 8;
+  } else if(data_size == 32){
+    imm.val = (uint32_t)(imm.val << 24) >> 24;
+  }
+  rm.val = alu_add(imm.val, rm.val);
+  operand_write(&rm);
+
+  return len;
+}
