c971cc9e716e76c819231c49584591666fe2a850
tracer-pa2017 <tracer@njuics.org>
2018-05-03 13:48:05 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  13:48:05 up 8 days,  3:10,  1 user,  load average: 0.06, 0.15, 0.07 517eac1bc4c185d561a76c7a8d77056825d6782

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -1,11 +1,9 @@
 #include "cpu/instr.h"
 
-static void instr_execute_1op_inc_pass()
+static void instr_execute_1op_inc()
 {
   operand_read(&opr_src);
-  // uint32_t temp_CF = cpu.eflags.CF;
   opr_src.val = alu_add(1, opr_src.val);
-  // cpu.eflags.CF = temp_CF;
   operand_write(&opr_src);
 }
 
