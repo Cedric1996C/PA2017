370a14cc76f643a20c93778561af981b86e76b35
tracer-pa2017 <tracer@njuics.org>
2018-05-04 19:25:57 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  19:25:57 up 8 days, 19:09,  1 user,  load average: 0.25, 0.16, 0.10 a6ce08def87321e2ee9f35fa81f21e24fb6431

--- nemu/src/cpu/instr/xor.c
+++ nemu/src/cpu/instr/xor.c
@@ -6,6 +6,8 @@ static void instr_execute_2op_xor()
   operand_read(&opr_dest);
   opr_dest.val = alu_xor(opr_src.val, opr_dest.val);
   operand_write(&opr_dest);
+  cpu.eflags.CF = 0;
+  cpu.eflags.OF = 0;
 }
 
 static void instr_execute_2op_or()
@@ -14,6 +16,8 @@ static void instr_execute_2op_or()
   operand_read(&opr_dest);
   opr_dest.val = alu_or(opr_src.val, opr_dest.val);
   operand_write(&opr_dest);
+  cpu.eflags.CF = 0;
+  cpu.eflags.OF = 0;
 }
 
 make_instr_impl_2op_or(xor, r, rm, v);
