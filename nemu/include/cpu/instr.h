17520f6c58680556c7fd963927b44fb3530ec6cc
tracer-pa2017 <tracer@njuics.org>
2018-05-04 20:19:00 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  20:19:00 up 8 days, 20:03,  1 user,  load average: 0.82, 0.25, 0.09 cd3a555ca577ac85c724e0c2d0edbdadc4f603c3

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -62,4 +62,6 @@ int pop_ebp_v(uint32_t eip, uint8_t opcode);
 int xor_r2rm_v(uint32_t eip, uint8_t opcode);
 int or_r2rm_v(uint32_t eip, uint8_t opcode);
 
+int cmp_i2rm_v(uint32_t eip, uint8_t opcode);
+
 #endif
--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -1,18 +1,28 @@
 #include "cpu/instr.h"
 
-make_instr_func(cmp_r2rm_v)
+static void instr_execute_2op_cmp()
 {
-  OPERAND reg, rm;
-  int len = 1;
-  reg.data_size = data_size;
-  rm.data_size = data_size;
-  len += modrm_r_rm(eip + 1, &reg, &rm);
-
-  operand_read(&reg);
-  operand_read(&rm);
-
-  rm.val = alu_sub(reg.val, rm.val);
-  print_asm_2("cmp", "", len, &reg, &rm);
-
-  return len;
+  operand_read(&opr_src);
+  operand_read(&opr_dest);
+  opr_dest.val = alu_sub(reg.val, rm.val);
 }
+
+make_instr_impl_2op_cmp(r, rm, v);
+make_instr_impl_2op_cmp(i ,rm ,v);
+
+// make_instr_func(cmp_r2rm_v)
+// {
+//   OPERAND reg, rm;
+//   int len = 1;
+//   reg.data_size = data_size;
+//   rm.data_size = data_size;
+//   len += modrm_r_rm(eip + 1, &reg, &rm);
+
+//   operand_read(&reg);
+//   operand_read(&rm);
+
+//   rm.val = alu_sub(reg.val, rm.val);
+//   print_asm_2("cmp", "", len, &reg, &rm);
+
+//   return len;
+// }
