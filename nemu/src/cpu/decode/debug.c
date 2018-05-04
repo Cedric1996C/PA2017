f1dd51eb6594f35dff1957ccc844ae71e74dac2a
tracer-pa2017 <tracer@njuics.org>
2018-05-04 19:29:26 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  19:29:26 up 8 days, 19:13,  1 user,  load average: 0.08, 0.12, 0.09 8d31d6029703ed48fb8895867ec0fa1384063f2

--- nemu/src/cpu/decode/debug.c
+++ nemu/src/cpu/decode/debug.c
@@ -41,7 +41,8 @@ void print_asm_2(char * instr, char * suffix, uint8_t len, OPERAND * opr_1, OPER
 			printf("%02x ", instr_fetch(cpu.eip + i, 1));
 		}
 		for(i = 0 ; i <= MAX_INSTR_BYTES - len ; i++) printf("   ");
-		printf("%s%s %s, %s\n", instr, suffix,addr_str_1, addr_str_2);
+			printf("%s%s %s, %s", instr, suffix,addr_str_1, addr_str_2);
+			printf("%s %s\n", opr_1.val, opr_2.val);
 	}
 }
 void print_asm_3(char * instr, char * suffix, uint8_t len, OPERAND * opr_1, OPERAND * opr_2, OPERAND * opr_3) {
