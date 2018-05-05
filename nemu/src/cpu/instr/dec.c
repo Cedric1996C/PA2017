43fc61f6975a2fcbbcb1f5b9ebdfef3c5a3a2ee5
tracer-pa2017 <tracer@njuics.org>
2018-05-05 17:35:31 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  17:35:31 up 9 days,  7:50,  1 user,  load average: 0.01, 0.03, 0.00 c5078c32da28b159323b5d53500b52ced60ba33a

--- nemu/src/cpu/instr/dec.c
+++ nemu/src/cpu/instr/dec.c
@@ -2,17 +2,17 @@
 
 static void instr_execute_1op_dec_reg()
 {
-  uint32_t temp_CF = cpu.eflags.CF;
+  // uint32_t temp_CF = cpu.eflags.CF;
   opr_src.val = alu_sub(1, opr_src.val);
-  cpu.eflags.CF = temp_CF;
+  // cpu.eflags.CF = temp_CF;
 }
 
 static void instr_execute_1op_dec()
 {
   operand_read(&opr_src);
-  uint32_t temp_CF = cpu.eflags.CF;
+  // uint32_t temp_CF = cpu.eflags.CF;
   opr_src.val = alu_sub(1, opr_src.val);
-  cpu.eflags.CF = temp_CF;
+  // cpu.eflags.CF = temp_CF;
   operand_write(&opr_src);
 }
 
