ebb0dfbfbc0047cee76f356fd97943bd57c8524c
tracer-pa2017 <tracer@njuics.org>
2018-05-05 20:57:12 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  20:57:12 up 9 days, 11:11,  1 user,  load average: 0.02, 0.07, 0.07 ff2f9516e25420a56a43daf633946d58193faa55

--- nemu/src/cpu/instr/cmps.c
+++ nemu/src/cpu/instr/cmps.c
@@ -53,9 +53,9 @@ make_instr_func(cmp_i2rm_bv)
 	operand_read(&imm);
 
 	// imm.data_size = 32;
-	// imm.val = sign_ext(imm.val, data_size); // sign extension
+	imm.val = sign_ext(imm.val, data_size); // sign extension
 
-	rm.val = alu_sub(rm.val, imm.val);
+	rm.val = alu_sub(imm.val, rm.val);
 	print_asm_2("cmp", "", len, &imm, &rm);
 	return len;
 }
