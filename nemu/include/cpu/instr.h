4c6f5fadbd7b44668d11949fd95d85d821be74f0
tracer-pa2017 <tracer@njuics.org>
2018-05-10 15:04:48 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  15:04:48 up 12 days,  5:42,  1 user,  load average: 0.08, 0.10, 0.07 deb5a0565e60e491de83f997b2ccdfb8576f1873

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -102,7 +102,7 @@ int pop_edi_v(uint32_t eip, uint8_t opcode);
 int ret_i_v(uint32_t eip, uint8_t opcode);
 int and_i2rm_v(uint32_t eip, uint8_t opcode);
 int test_i2rm_v(uint32_t eip, uint8_t opcode);
-// int pop_esi_v(uint32_t eip, uint8_t opcode);
+int test_i2eax_v(uint32_t eip, uint8_t opcode);
 // int pop_edi_v(uint32_t eip, uint8_t opcode);
 // int ret_i_v(uint32_t eip, uint8_t opcode);
 // int and_i2rm_v(uint32_t eip, uint8_t opcode);
--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -13,3 +13,24 @@ static void instr_execute_2op()
 make_instr_impl_2op(test, r, rm, b)
 make_instr_impl_2op(test, r, rm, v)
 make_instr_impl_2op(test, i, rm, v)
+
+make_instr_func(test_i2eax_v){
+  OPERAND imm, eax;
+  int len = 1;
+  imm.type = OPR_IMM;
+  imm.data_size = data_size;
+  imm.addr = eip + len;
+  len += 1;
+  eax.type = OPR_REG;
+  eax.addr = REG_EAX;
+  eax.data_size = data_size;
+  
+  print_asm_2("test", "", len, &imm, &eax);
+  operand_read(&imm);
+  operand_read(&eax);
+  imm.val = alu_sub(imm.val, eax.val);
+  cpu.eflags.CF = 0;
+  cpu.eflags.OF = 0;
+
+  return len;
+}
