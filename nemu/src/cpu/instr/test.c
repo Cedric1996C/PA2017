d589f493993699364c46f63173a705babe642892
tracer-pa2017 <tracer@njuics.org>
2018-05-10 10:45:28 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  10:45:28 up 12 days,  1:30,  1 user,  load average: 0.14, 0.14, 0.10 8808bafb250f5bc514ca3ef6b507831a750d8fc1

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -13,20 +13,3 @@ static void instr_execute_2op()
 make_instr_impl_2op(test, r, rm, b)
 make_instr_impl_2op(test, r, rm, v)
 
-// make_instr_func(test_r2rm_v)
-// {
-//   OPERAND reg, rm;
-//   int len = 1;
-  
-//   reg.data_size = data_size;
-//   rm.data_size = data_size;
-//   len += modrm_r_rm(eip+1, &reg, &rm);
-//   operand_read(&reg);
-//   operand_read(&rm);
-//   print_asm_2("test", "", len, &reg, &rm);
-//   rm.val = alu_and(reg.val, rm.val);
-//   cpu.eflags.CF = 0;
-//   cpu.eflags.OF = 0;
-  
-//   return len;
-// }
