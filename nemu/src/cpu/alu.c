eb02554b6aff8028314e745046186a0872ef539d
tracer-pa2017 <tracer@njuics.org>
2018-05-10 10:25:37 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  10:25:37 up 12 days,  1:10,  1 user,  load average: 0.16, 0.06, 0.06 4897b3947b830bbf5f8e13d2d19a4a79f495aede

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -63,7 +63,7 @@ void set_CF_sub(uint32_t src, uint32_t dest){
 
 void set_OF_sub(uint32_t src, uint32_t dest){
   uint32_t src_neg = ~(src+1) + 1;
-  set_OF_add(src_neg + dest, src_neg, dest);
+  set_OF_add(dest-src, src_neg, dest);
 }
 
 uint32_t alu_sub(uint32_t src, uint32_t dest) {
