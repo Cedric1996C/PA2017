5b1f811c693a0497a22b54b17c1aa6d1c3f55829
tracer-pa2017 <tracer@njuics.org>
2018-05-10 09:05:19 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  09:05:19 up 11 days, 23:59,  1 user,  load average: 0.35, 0.29, 0.14 9dbc368a7e7333d886ffe436630fab5aa74d6423

--- nemu/src/cpu/instr/cmps.c
+++ nemu/src/cpu/instr/cmps.c
@@ -4,7 +4,9 @@ static void instr_execute_2op()
 {
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
+	print_asm_2("cmp", "", 2, &opr_src, &opr_dest);
 	opr_dest.val = alu_sub(opr_src.val, opr_dest.val);
+	print_asm_2("cmp", "", 2, &opr_src, &opr_dest);
 }
 
 make_instr_impl_2op(cmp, r, rm, v);
