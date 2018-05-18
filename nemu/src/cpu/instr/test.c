387c05a62b5d789d39d1aff9d17fac40283b0e61
tracer-pa2017 <tracer@njuics.org>
2018-05-18 10:28:10 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  10:28:10 up 12 days, 14:24,  1 user,  load average: 0.91, 0.22, 0.10 f9e2672b492222c8ae999d93ea567aaafa45b74

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -5,7 +5,6 @@ static void instr_execute_2op()
   operand_read(&opr_src);
   operand_read(&opr_dest);
   opr_dest.val = alu_and(opr_src.val, opr_dest.val);
-  operand_write(&opr_dest);
   cpu.eflags.OF = 0;
   cpu.eflags.CF = 0;
 }
