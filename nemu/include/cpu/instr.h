56edfe5c17612576a3798652d7ec03ba1b7dd194
tracer-pa2017 <tracer@njuics.org>
2018-05-18 11:23:12 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  11:23:12 up 12 days, 15:19,  1 user,  load average: 0.04, 0.14, 0.11 4443d339ac148bf713404793e019c77a781b8b1

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -122,6 +122,10 @@ int jp_v(uint32_t eip, uint8_t opcode);
 int jnp_v(uint32_t eip, uint8_t opcode);
 int jge_v(uint32_t eip, uint8_t opcode);
 int jg_v(uint32_t eip, uint8_t opcode);
+int jo_v(uint32_t eip, uint8_t opcode);
+int jno_v(uint32_t eip, uint8_t opcode);
+int jb_v(uint32_t eip, uint8_t opcode);
+int jae_v(uint32_t eip, uint8_t opcode);
 
 int inc_REG_ESP_v(uint32_t eip, uint8_t opcode);
 int inc_REG_EBP_v(uint32_t eip, uint8_t opcode);
--- nemu/src/cpu/instr/jcc.c
+++ nemu/src/cpu/instr/jcc.c
@@ -41,6 +41,10 @@ make_instr_impl_1op_cc(j, i, v, p);
 make_instr_impl_1op_cc(j, i, v, np);
 make_instr_impl_1op_cc(j, i, v, ge);
 make_instr_impl_1op_cc(j, i, v, g);
+make_instr_impl_1op_cc(j, i, v, o);
+make_instr_impl_1op_cc(j, i, v, no);
+make_instr_impl_1op_cc(j, i, v, b);
+make_instr_impl_1op_cc(j, i, v, ae);
 
 make_instr_func(jl_b)
 {
