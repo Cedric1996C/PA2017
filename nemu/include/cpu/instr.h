b5a4957f3d0eb211a9a35e30b7d3bcf0cbba6298
tracer-pa2017 <tracer@njuics.org>
2018-05-18 11:50:27 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  11:50:27 up 12 days, 15:47,  1 user,  load average: 0.00, 0.02, 0.01 4dc151891523f0e9e610553f48aa62f8fb1893d3

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -126,12 +126,13 @@ int jo_v(uint32_t eip, uint8_t opcode);
 int jno_v(uint32_t eip, uint8_t opcode);
 int jb_v(uint32_t eip, uint8_t opcode);
 int jae_v(uint32_t eip, uint8_t opcode);
+
 int inc_REG_ESP_v(uint32_t eip, uint8_t opcode);
 int inc_REG_EBP_v(uint32_t eip, uint8_t opcode);
 int inc_REG_ESI_v(uint32_t eip, uint8_t opcode);
 int adc_i2al_b(uint32_t eip, uint8_t opcode);
 int dec_ebp_v(uint32_t eip, uint8_t opcode);
 int adc_r2rm_b(uint32_t eip, uint8_t opcode);
-int add_r2rm_b(uint32_t eip, uint8_t opcode);
+int and_i2rm_b(uint32_t eip, uint8_t opcode);
 
 #endif
