1a1b025735d2e422954420ba81e53ef2a1ec7be0
tracer-pa2017 <tracer@njuics.org>
2018-05-18 10:45:03 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  10:45:03 up 12 days, 14:41,  1 user,  load average: 0.06, 0.04, 0.04 70edb0e64797bb4e7f18f5f755bb6dbf391658c

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -111,7 +111,7 @@ int cmps_v(uint32_t eip, uint8_t opcode);
 int cmps_b(uint32_t eip, uint8_t opcode);
 
 int inc_REG_EDI_v(uint32_t eip, uint8_t opcode);
-// int stos_b(uint32_t eip, uint8_t opcode);
+int test_i2rm_b(uint32_t eip, uint8_t opcode);
 // int test_i2al_b(uint32_t eip, uint8_t opcode);
 // int cmps_v(uint32_t eip, uint8_t opcode);
 // int cmps_b(uint32_t eip, uint8_t opcode);
--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -12,6 +12,7 @@ static void instr_execute_2op()
 make_instr_impl_2op(test, r, rm, b)
 make_instr_impl_2op(test, r, rm, v)
 make_instr_impl_2op(test, i, rm, v)
+make_instr_impl_2op(test, i, rm, b)
 
 make_instr_func(test_i2eax_v){
   OPERAND imm, eax;
