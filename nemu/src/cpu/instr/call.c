244fd13925d6f799712ce314c192f59cd28b2912
tracer-pa2017 <tracer@njuics.org>
2018-05-20 09:47:19 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  09:47:19 up 12 days, 21:17,  1 user,  load average: 0.06, 0.03, 0.04 ef1a4f8e445acaf01896815bae7521ad3492e049

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -11,19 +11,19 @@ make_instr_func(call_rel_v)
   temp_eip.data_size = data_size;
   temp_eip.val = eip+len;
   temp_eip.addr = cpu.esp;
-  opr_eip.sreg = SREG_SS;
+  temp_eip.sreg = SREG_SS;
   operand_write(&temp_eip);
 
   rel.type = OPR_IMM;
   rel.data_size = data_size;
   rel.addr = eip + 1;
   operand_read(&rel);
-  print_asm_1("call", "", len, &imm);
+  print_asm_1("call", "", len, &rel);
 
   if (data_size == 16){
-    imm.val = (int32_t)(imm.val << 16) >> 16;
+    rel.val = (int32_t)(rel.val << 16) >> 16;
   }
-  len += (int32_t)imm.val;
-  
+  len += (int32_t)rel.val;
+
   return len;
 }
