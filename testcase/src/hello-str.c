ebc47be0028530ca9f8d1eea313c02039dd17fdf
tracer-pa2017 <tracer@njuics.org>
2018-05-20 09:31:15 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  09:31:15 up 12 days, 21:01,  1 user,  load average: 0.21, 0.28, 0.13 dbb64dd311fe6d35c6ad389c897b2ae193c8f7bb

--- testcase/src/hello-str.c
+++ testcase/src/hello-str.c
@@ -14,14 +14,15 @@ void *sbrk(int incr) {
 }
 
 int main() {
-	// sprintf(buf, "%s", "Hello world!\n");
-	nemu_assert(strcmp("Hello world!\n", "Hello world!\n") == 0);
+	sprintf(buf, "%s", "Hello world!\n");
+	assert(0)
+	nemu_assert(strcmp(buf, "Hello world!\n") == 0);
 
-	// sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
-	// nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
+	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
+	nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
 
-	// sprintf(buf, "%d + %d = %d\n", 2, 10, 12);
-	// nemu_assert(strcmp(buf, "2 + 10 = 12\n") == 0);
+	sprintf(buf, "%d + %d = %d\n", 2, 10, 12);
+	nemu_assert(strcmp(buf, "2 + 10 = 12\n") == 0);
 
 	HIT_GOOD_TRAP;
 	return 0;
