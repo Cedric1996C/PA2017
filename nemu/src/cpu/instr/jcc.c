94b096bc298a95a3a4981e25e4e01060d130a09c
tracer-pa2017 <tracer@njuics.org>
2018-05-20 15:00:29 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  15:00:29 up 13 days,  2:30,  1 user,  load average: 0.07, 0.06, 0.03 63997f303c2a2026feafbce645f27008d378d435

--- nemu/src/cpu/instr/jcc.c
+++ nemu/src/cpu/instr/jcc.c
@@ -46,7 +46,7 @@ make_instr_impl_1op_cc(j, i, v, no);
 make_instr_impl_1op_cc(j, i, v, b);
 make_instr_impl_1op_cc(j, i, v, ae);
 
-// make_instr_impl_1op_cc(j, i, b, a);
+make_instr_impl_1op_cc(j, i, b, a);
 make_instr_impl_1op_cc(j, i, b, be);
 make_instr_impl_1op_cc(j, i, b, l);
 make_instr_impl_1op_cc(j, i, b, le);
