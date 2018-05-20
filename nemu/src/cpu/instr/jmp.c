4aff733a641bfe88cbf6cbc25dd04efd09f9dc3e
tracer-pa2017 <tracer@njuics.org>
2018-05-20 14:40:19 +0800

    >  nemu 141220045 cong Linux robottdog 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  14:40:19 up 13 days,  2:10,  1 user,  load average: 0.02, 0.04, 0.03 8fedcdbc44ce15e668e38ed1919c1071f93a9c2a

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -1,6 +1,5 @@
 #include "cpu/instr.h"
 
-
 make_instr_func(jmp_near){
   OPERAND rel;
   rel.type = OPR_IMM;
@@ -18,7 +17,7 @@ make_instr_func(jmp_near){
   return 1 + data_size / 8;
 }
 
-/*
+
 make_instr_func(jmp_rel_v)
 {
   OPERAND imm;
@@ -74,66 +73,4 @@ make_instr_func(jmp_rel_b)
   len += (uint8_t)imm.val;
 
   return len;
-}
-*/
-
-make_instr_func(jmp_rel_v)
-{
-  OPERAND imm;
-
-  int len = 1;
-  imm.type = OPR_IMM;
-  imm.addr = eip + len;
-  len += data_size / 8;
-  imm.data_size = data_size;
-  print_asm_1("jmp rel", "", len, &imm);
-  operand_read(&imm);
-
-  assert(imm.data_size == 32 || imm.data_size == 16);
-  if (imm.data_size == 16)
-    len += (int32_t)(imm.val << 16) >> 16;
-  else
-    len += imm.val;
-  return len;
-}
-
-make_instr_func(jmp_rel_b)
-{
-  OPERAND imm;
-  int len = 1;
-
-  imm.type = OPR_IMM;
-  imm.addr = eip + len;
-  imm.data_size = 8;
-  len += 1;
-
-  operand_read(&imm);
-  print_asm_1("jmp", "", len, &imm);
-  len += (int8_t)imm.val;
-
-  return len;
-}
-
-make_instr_func(jmp_rm_v)
-{
-  OPERAND rm;
-
-  int len = 1;
-  rm.data_size = data_size;
-
-  len += modrm_rm(eip + len, &rm);
-  operand_read(&rm);
-
-  print_asm_1("jmp rm", "", len, &rm);
-
-  assert(data_size == 16 || data_size == 32);
-  if (data_size == 16)
-  {
-    rm.val = (int32_t)(rm.val << 16) >> 16;
-    cpu.eip = rm.val & 0xffff;
-  }
-  else
-    cpu.eip = rm.val;
-
-  return 0;
 }
